40364da7d436d40ba2cdef7103f855cdb1e5f403
Asael <zanduva@gmail.com>
2023-05-17 13:02:09 -0600

    cardtitles

--- carddetail/src/App.jsx
+++ carddetail/src/App.jsx
@@ -7,19 +7,28 @@ function App() {
 
   const [cardholder, setCardholder] = useState('')
 
+
   const onChangeCardholder = (e) => {
-      e.preventDefault()
-      setCardholder({
-        [e.target.name] : e.target.value
-      })
+    e.preventDefault()
+    setCardholder({
+      ...cardholder,
+      [e.target.name]: e.target.value
+    })
+
   }
+
 
   console.log(cardholder)
   return (
     <div className="App">
       <section className='cards'>
         <img src={front} alt='front' className='front' />
-        <p>{cardholder.name}</p>
+        <div className='info_front'>
+          <p className='cardholder_number'>{cardholder.number ? cardholder?.number?.toUpperCase().replace(/\w{4}(?=\w)/g, '$& ') : '9999 9999 9999 9999'}</p>
+          <p className='cardholder_name'>{cardholder.name ? cardholder?.name?.toUpperCase() : 'JANE APPLESEED'}</p>
+          <p className='dat'></p>
+        </div>
+
         <img src={back} alt='back' className='back' />
       </section>
 
@@ -27,10 +36,10 @@ function App() {
       <form className='forms'>
         <div className='inputs'>
           <label htmlFor='name'>CARDHOLDER NAME</label>
-          <input type='text' name='name' placeholder='e.g. Jane Appleseed' value={cardholder.name} onChange={onChangeCardholder}/>
+          <input type='text' name='name' placeholder='e.g. Jane Appleseed' value={cardholder.name} onChange={onChangeCardholder} />
 
           <label htmlFor='number'>CARD NUMBER</label>
-          <input type='text' name='number' placeholder='e.g. 1234 5678 9123 0000' />
+          <input type='text' name='number' placeholder='e.g. 1234 5678 9123 0000' value={cardholder.number} onChange={onChangeCardholder} maxLength={16}/>
 
           <div className='labels'>
             <label htmlFor='date'>EXP. DATE (MM/YY)</label>
@@ -38,9 +47,9 @@ function App() {
           </div>
 
           <div className='date_input'>
-            <input type='text' name='date' placeholder='MM' className='MMYY' />
-            <input type='text' name='date' placeholder='YY' className='MMYY' />
-            <input type='text' name='cvv' placeholder='e.g. 123' />
+            <input type='text' name='date' placeholder='MM' className='MMYY' maxLength={2}/>
+            <input type='text' name='date' placeholder='YY' className='MMYY' maxLength={2}/>
+            <input type='text' name='cvv' placeholder='e.g. 123' maxLength={3}/>
           </div>
           <input type='submit' value='Confirm' className='submit' />
         </div>
